a161b1e3a2a2d7be864d3f253ea4592df2cd7bac
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2020-11-15 13:43:43 +1100

    WIP - VPN Launch handler Lambda code

--- Lambda/VPNLaunchedHandler/VPNLaunchedHandler.py
+++ Lambda/VPNLaunchedHandler/VPNLaunchedHandler.py
@@ -1,5 +1,28 @@
+import json
+import jmespath
+import boto3
 
-def vpn_launched_handler(event, context):
-    #FIXME: Actually Handle Launch Event Messages
-    print("EVENT: %s" % event)
-    print("CONTEXT: %s" % context)
+ec2 = boto3.client('ec2')
+
+def vpn_launched(instanceid):
+    print("Instance Launched: %s\n" % instanceid)
+    vpn_instance = ecc.Instance(instanceid)
+    
+    vpn_instance.wait_until_running()
+    print("Instance Running: %s\n" % instanceid)
+
+def vpn_stopped(instanceid):
+    print("Instance Stopped: %s\n" % instanceid)
+
+def notification_handler(event, context):
+    for message_raw in jmespath.search('Records[*].Sns.Message', event):
+        message_json = json.loads(message_raw)
+        
+        (instanceid, event) = jmespath.search('[EC2InstanceId, Event]', message_json)
+        
+        if event == 'autoscaling:EC2_INSTANCE_LAUNCH':
+            vpn_launched(instanceid)
+        elif event == 'autoscaling:EC2_INSTANCE_TERMINATE':
+            vpn_stopped(instanceid)
+        else:
+            print('Unhandled Event: %s\n' % event)
