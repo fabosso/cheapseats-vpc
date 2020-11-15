48fda9edbfffe70df2fec928f81e18186440d2fe
Adam Luchjenbroers <adam.luchjenbroers@gmail.com>
2020-11-15 21:03:02 +1100

    First cut - VPN Start / Stop handler

--- Lambda/VPNLaunchedHandler/VPNLaunchedHandler.py
+++ Lambda/VPNLaunchedHandler/VPNLaunchedHandler.py
@@ -2,20 +2,54 @@ import json
 import jmespath
 import boto3
 
-ec2 = boto3.client('ec2')
+from os import environ
 
+ec2 = boto3.client('ec2')
+ssm = boto3.client('ssm')
+
+vpc = environ.get('VPC_ID')
+vpcname = environ.get('VPC_NAME')
+
+def get_ssm_parameter(param):
+    json = ssm.get_parameter(Name=param)
+    
+    return jmespath.search('Parameter.Value', json)
+
+def list_target_routes()
+    routes_json = ec2.describe_route_tables(Filters=[{'Name' : 'tag:VPN-Accessible', 'Values' : ['Yes', 'True']},
+                                                     {'Name' : 'vpc-id', 'Values' : [vpc]}])
+    return jmespath.search('RouteTables[*].RouteTableId', routes_json)
+    
 def vpn_launched(instanceid):
     print("Instance Launched: %s\n" % instanceid)
     vpn_instance = ecc.Instance(instanceid)
+    vpn_cidr = get_ssm_parameter('/%s/Vpn/HomeNetworkCIDR' % vpcname)
     
     vpn_instance.wait_until_running()
     print("Instance Running: %s\n" % instanceid)
     
     # Disable Source / Destination check to allow this box to act as an OpenVPN router.
     ec2.modify_instance_attribute(InstanceId=instanceid, SourceDestCheck={'Value': False})
+    print("Source Destination Check disabled")
+    
+    for routeTableId in list_target_routes():
+        print("Adding VPN route for Route Table %s" % routeTableId)
+        ec2.create_route(RouteTableId = routeTableId, DestinationCidrBlock = vpn_cidr, InstanceId = instanceid)
 
 def vpn_stopped(instanceid):
     print("Instance Stopped: %s\n" % instanceid)
+    
+    vpn_cidr = get_ssm_parameter('/%s/Vpn/HomeNetworkCIDR' % vpcname)
+    for routeTableId in list_target_routes():
+        print("Removing VPN route for Route Table %s" % routeTableId)
+        try:
+            ec2.delete_route(RouteTableId = routeTableId, DestinationCidrBlock = vpn_cidr)
+        except ClientError as e:
+            # We expect the occasional failure where the route doesn't exist - this can be safely ignored.
+            if e.response['Error']['Code'] != 'InvalidRoute.NotFound':
+                raise e
+            else:
+                print("No route to remove")
 
 def notification_handler(event, context):
     for message_raw in jmespath.search('Records[*].Sns.Message', event):
